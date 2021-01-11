ac94a2cae57ea0a1303a5b25b2805529d251fee1
김준 <xbfld0614@gmail.com>
2021-01-11 15:07:38 +0900

    정리

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -4,137 +4,141 @@ let d3 = require('d3-contour');
 let sdf = require('bitmap-sdf');
 
 class Client {
+    private canvas: HTMLCanvasElement;
+
     constructor() {
         const playGround: HTMLDivElement = document.getElementById('playGround') as HTMLDivElement;
-        const canvas: HTMLCanvasElement = document.createElement('canvas');
-        const sample = new Image();
-        playGround.appendChild(canvas);
+        this.canvas = document.createElement('canvas');
+        playGround.appendChild(this.canvas);
+        this.draw();
+    }
 
-        function draw() {
+    private loadImage(src: string): Promise<HTMLImageElement> {
+        const image: HTMLImageElement = new Image();
 
-            let targets0: number[] = []
-            let targets1 = [-30]
-            let targets3 = [-15, -30, -45]
-            let targets5 = [0, -10, -20, -30, -40]
-            let targets30 = [
-                0, -10, -20, -30, -40, -50, -60, -70, -80, -90,
-                -3, -13, -23, -33, -43, -53, -63, -73, -83, -93,
-                -6, -16, -26, -36, -46, -56, -66, -76, -86, -96,
-            ]
+        return new Promise<HTMLImageElement>((resolve, reject) => {
+            image.onload = () => resolve(image);
+            image.onerror = (e) => reject(e);
+            image.src = src;
+        });
+    }
 
-            let targets = targets5.map(x => x);
+    private async draw() {
+        const targets0: number[] = []
+        const targets1 = [-30]
+        const targets2 = [-30, -35]
+        const targets3 = [-15, -30, -45]
+        const targets5 = [0, -10, -20, -30, -40]
+        const targets30 = [
+            0, -10, -20, -30, -40, -50, -60, -70, -80, -90,
+            -3, -13, -23, -33, -43, -53, -63, -73, -83, -93,
+            -6, -16, -26, -36, -46, -56, -66, -76, -86, -96,
+        ]
 
+        const targets: Array<number> = targets2.map(x => x);
 
-            sample.onload = () => {
+        const srcPath = "./resources/image/sample_400_500.png";
+        //const srcPath = "./resources/image/sample_1000_1000.png";
+        //const srcPath = "./resources/image/sample_5000_5000.png";
 
-                let ctx = canvas.getContext("2d");
-                var contours = createContours(ctx, targets);
+        const image: HTMLImageElement = await this.loadImage(srcPath);
+        const ctx = this.canvas.getContext('2d');
 
-                ctx.lineWidth = 3;
-                ctx.strokeStyle = '#000'
-
-                drawContours(contours, ctx);
-
-                // drawContours(contours, ctx, ()=>{});
-                // drawContours(contours, ctx, (ring) => console.log(ring));
-                console.log(contours);
-            }
-
-            function createContours(ctx: CanvasRenderingContext2D, targets: number[]) {
-                // 외각선 잘림 방지를 위해 원본 그림 주위에 여백을 둔다
-                let margin = Math.ceil(
-                    targets
-                        .map(Math.abs)
-                        .reduce((acc, val) => Math.max(acc, val)))
-
-                var n, m;
-                n = sample.width + margin * 2;
-                m = sample.height + margin * 2;
-
-                ctx.canvas.width = n;
-                ctx.canvas.height = m;
-
-                console.assert(ctx.canvas.width >= n)
-                console.assert(ctx.canvas.height >= m)
+        const contours = this.createContours(ctx, targets, image);
+        this.drawContours(contours, ctx);
 
 
-                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        // this.drawContours(contours, ctx, ()=>{});
+        // this.drawContours(contours, ctx, (ring) => console.log(ring));
+        console.log(contours);
+    }
 
+    /**등고선을 그린다.*/
+    private drawContours(contours: any, ctx: CanvasRenderingContext2D, drawer = this.drawLinearring) {
+        contours.forEach((contour: { value: number; coordinates: number[][][][]; }) => {
 
-                //샘플 이미지를 그린다.
-                ctx.drawImage(sample, margin, margin);
+            contour.coordinates.forEach((polygon: number[][][]) => {
+                //polygon은 linearring의 배열로 첫 요소는 가장 바깥 폐곡선이다
 
+                //가장 바깥 테두리만 가져오기
+                polygon = polygon.slice(0, 1);
 
-                const opaque = 1;
-                const transparent = 0;
-                var values = Float32Array.from(
-                    ctx.getImageData(0, 0, n, m)
-                        .data
-                        .filter((elem, index) => index % 4 == 3) //Take alpha channel
-                        .map((alpha: number) => alpha < 0.5 ? transparent : opaque)
-                );
-                // console.log(values)
-
-                // 라이브러리 bitmap-sdf에서 거리 값을 임의로 [0,1]범위에 맞춘다
-                // https://github.com/dy/bitmap-sdf/blob/master/index.js#L86
-                // 따라서 거리 값을 온전히 얻고 싶다면 radius와 cutoff를 적절히 설정해야 한다.
-                // radius는 적당히 큰 수로 그림에서 등고선까지 거리보다 크면 된다.
-                // cutoff는 0.5; [0,1] 사이의 값으로 radius의 기준이 된다.
-                let radius = 100000;
-                let cutoff = 0.5;
-
-                // values는 Float32Array로 할 것
-                // Uint8ClampedArray일 경우 distance가 의도치 않은 손실 발생
-                let distance = sdf(values, { radius: radius, width: n, height: m, cutoff: cutoff });
-                // console.log(distance)
-                // [0,1]범위로 축소된 거리를 복원하는 함수
-                let recoverDistance = (x: number) => (x - cutoff) * radius;
-                distance = distance.map(recoverDistance);
-
-                // 등고선 적용
-                var contours = d3.contours()
-                    .size([n, m])
-                    .thresholds(targets)(distance);
-                return contours;
-            }
-
-            /**등고선을 그린다.*/
-            function drawContours(contours: any, ctx: CanvasRenderingContext2D, drawer = drawLinearring) {
-                contours.forEach((contour: { value: number; coordinates: any[][]; }) => {
-                    let hue = (contour.value * 5) % 360;
-                    ctx.strokeStyle = 'hsl(' + hue + ',100%,50%)';
-
-                    contour.coordinates.forEach((polygon: any[]) => {
-                        //polygon은 linearring의 배열로 첫 요소는 가장 바깥 폐곡선이다
-
-                        //가장 바깥 테두리만 가져오기
-                        polygon = polygon.slice(0, 1);
-
-                        polygon.forEach((linearring: any[]) => {
-                            drawer(linearring, ctx);
-                        });
-                    });
+                polygon.forEach((linearring: number[][]) => {
+                    drawer(linearring, ctx);
                 });
-            }
+            });
+        });
+    }
 
-            //폐곡선 그리기
-            function drawLinearring(linearring: any[], ctx: CanvasRenderingContext2D) {
-                ctx.beginPath();
-                let last = linearring[linearring.length - 1];
-                ctx.moveTo(last[0], last[1]);
-                linearring.forEach((point: number[]) => {
-                    ctx.lineTo(point[0], point[1]);
-                });
-                ctx.closePath();
-                ctx.stroke();
-            }
-        }
+    //폐곡선 그리기
+    //input: Array of points
+    //effect: Draw a linear-ring on the CanvasRenderingContext2D
+    private drawLinearring(linearring: Array<Array<number>>, ctx: CanvasRenderingContext2D): void {
+        ctx.save();
 
-        draw();
-        sample.src = "./resources/image/sample_400_500.png";
-        // sample.src = "./resources/image/sample_1000_1000.png";
-        // sample.src = "./resources/image/sample_5000_5000.png";
+        ctx.beginPath();
+        const last = linearring[linearring.length - 1];
+        ctx.moveTo(last[0], last[1]);
+        linearring.forEach((point: number[]) => {
+            ctx.lineTo(point[0], point[1]);
+        });
+        ctx.closePath();
+        ctx.stroke();
 
+        ctx.restore();
+    }
+
+    //side-effect: 캔버스 더러워짐
+    private createContours(ctx: CanvasRenderingContext2D, targets: Array<number>, image: HTMLImageElement) {
+
+        const maxAbsoluteValueOfTargets = Math.ceil(
+            targets
+                .map(Math.abs)
+                .reduce((acc, val) => Math.max(acc, val)));
+
+        // 외각선 잘림 방지를 위해 원본 그림 주위에 여백을 둔다
+        const margin = maxAbsoluteValueOfTargets;
+        const width = image.width + margin * 2;
+        const height = image.height + margin * 2;
+
+        ctx.canvas.width = width;
+        ctx.canvas.height = height;
+
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        //샘플 이미지를 그린다.
+        ctx.drawImage(image, margin, margin);
+
+        const opaque = 1;
+        const transparent = 0;
+        const values = Float32Array.from(
+            ctx.getImageData(0, 0, width, height)
+                .data
+                .filter((elem, index) => index % 4 == 3) //Take alpha channel
+                .map((alpha: number) => alpha < 0.5 ? transparent : opaque)
+        );
+
+        // 라이브러리 bitmap-sdf에서 거리 값을 임의로 [0,1]범위에 맞춘다
+        // https://github.com/dy/bitmap-sdf/blob/master/index.js#L86
+        // 따라서 거리 값을 온전히 얻고 싶다면 radius와 cutoff를 적절히 설정해야 한다.
+        // radius는 적당히 큰 수로 그림에서 등고선까지 거리의 두배 보다 크면 된다.
+        // cutoff는 0.5; [0,1] 사이의 값으로 radius의 기준이 된다.
+        const radius = margin * 16
+        const cutoff = 0.5;
+
+        // values는 Float32Array로 할 것
+        // Uint8ClampedArray일 경우 distance가 의도치 않은 손실 발생
+        let distance = sdf(values, { radius: radius, width: width, height: height, cutoff: cutoff });
+
+        // [0,1]범위로 축소된 거리를 복원하는 함수
+        let recoverDistance = (x: number) => (x - cutoff) * radius;
+        distance = distance.map(recoverDistance);
+
+        // 등고선 적용
+        const contours = d3.contours()
+            .size([width, height])
+            .thresholds(targets)(distance);
+        return contours;
     }
 }
 
