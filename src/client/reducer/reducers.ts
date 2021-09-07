3474ee790839338a06b26c31ba45b1e4be14ae03
장수홍 <shjang@miridih.com>
2021-09-07 19:17:56 +0900

    pipe로는 안된다

--- src/client/reducer/reducers.ts
+++ src/client/reducer/reducers.ts
@@ -1,6 +1,5 @@
 import {Action, AnyAction, combineReducers } from "redux";
 import undoable, {groupByActionTypes} from "redux-undo";
-import { increateAction } from "../action/Action";
 import {initialState} from "../State/state";
 
 function counter(state = initialState.count, action: AnyAction) {
@@ -34,15 +33,31 @@ function colorReducer(state = initialState.color, action: AnyAction) {
     }
 };
 
+function pipeReducer(state = initialState.pipe, action: Action): boolean {
+    switch (action.type) {
+        case 'pipe':
+            return !state;
+        default:
+            return state;
+    }
+}
+
 export default combineReducers({
     color: colorReducer,
     count: undoable(counter, {
+        // filter: (action, currentState, previousHistory) => {
+        //     console.log(action, currentState, previousHistory);
+        //     return true;
+        // },
+        // groupBy: (action, currentState, previousHistory) => {
+        //     console.log(action, currentState, previousHistory);
+        // }
         groupBy: groupByActionTypes([
             'SETCOUNT'
         ])
     }),
-    // count: counter,
     // page: combineReducers({
     //     items: itemsReducer
     // })
+    pipe: pipeReducer
 });
