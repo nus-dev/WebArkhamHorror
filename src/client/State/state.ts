f469706a97c491403ea6e2ffb54bd55e2c0fe85f
장수홍 <shjang@miridih.com>
2021-09-07 19:18:01 +0900

    전체커멧

--- src/client/State/state.ts
+++ src/client/State/state.ts
@@ -6,4 +6,5 @@ export const initialState = {
     // page: {
     //     items: [] as Array<Item>
     // }
+    pipe: true
 };
--- src/client/view/CounterView.tsx
+++ src/client/view/CounterView.tsx
@@ -10,7 +10,10 @@ export function CounterView() {
             <h1>{count.present}</h1>
             <button onClick={increaseCount}>IncreaseAction</button>
             <button onClick={decreaseCount}>DecreaseAction</button>
-            <input type="range" min="1" max="100" onChange={(e) => setCount(Number(e.target.value))} value={count.present} className="slider" id="myRange" />
+            <input type="range" min="1" max="100" onMouseUp={() => {
+                console.log('pipe');
+                Store.dispatch({type: 'pipe'})
+            }} onChange={(e) => setCount(Number(e.target.value))} value={count.present} className="slider" id="myRange" />
             <button onClick={undo}>UNDO</button>
             <button onClick={redo}>REDO</button>
         </div>
